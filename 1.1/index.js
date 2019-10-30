bc4d950448e395caa4be8a231e76e85c884712d9
Alex Botham <Alex@Alexs-MBP.wlan>
2019-10-30 11:44:50 +0200

    index.js updated

--- 1.1/index.js
+++ 1.1/index.js
@@ -3,34 +3,34 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   
-  const Header = () => {
+  const Header = (props) => {
     return(
-        <h1>{course}</h1>
+        <h1>{props.course}</h1>
     )}
 
-    const Content = () => {
+    const Content = (props) => {
         return(
             <><p>
-            {part1} {exercises1}
+            {props.part1} {props.exercises1}
             </p>
             <p>
-            {part2} {exercises2}
+            {props.part2} {props.exercises2}
             </p>
             <p>
-            {part3} {exercises3}
+            {props.part3} {props.exercises3}
             </p></>
         )
     }
 
-    const Total = () => {
+    const Total = (props) => {
         return(
             <>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <p>Number of exercises {props.total}</p>
             </>
         )
     }
   
-const course = 'Half Stack application development'
+  const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -40,9 +40,9 @@ const course = 'Half Stack application development'
 
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+      <Total total={ exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
